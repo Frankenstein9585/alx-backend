d95c565a499a2f95e77990970a7609c72f01c565
Frankenstein9585 <donny9585@gmail.com>
2023-08-23 19:41:19 +0100

    Implemented LRUCache class

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""LIFO Cache Implementation"""
+"""LRU Cache Implementation"""
 
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
-    """LIFOCache Class inherits from BaseCaching"""
-    last_added = None
-
+    """LRUCache Class inherits from BaseCaching"""
     def __init__(self):
         """Initialize"""
         super().__init__()
@@ -16,7 +14,7 @@ class LRUCache(BaseCaching):
         self.cache = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item to the dictionary based on LIFO algorithm"""
+        """Adds an item to the dictionary based on LRU algorithm"""
         if key and item:
             if (len(self.cache_data) >= BaseCaching.MAX_ITEMS
                     and key not in self.cache_data):
@@ -42,31 +40,3 @@ class LRUCache(BaseCaching):
                 self.cache.move_to_end(key)
             print(self.cache)
             return self.cache_data.get(key)
-
-
-my_cache = LRUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
