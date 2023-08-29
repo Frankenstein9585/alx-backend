988c4667c46ce5dbd83ab9cef73c8236ee1943b1
Donald Peters <donny9585@gmail.com>
2023-08-29 21:29:46 +0100

    Done task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Flask Application"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get_locale() method"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index Route"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
